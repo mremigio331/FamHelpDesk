b39a82f2c797984f133fdc3d564318c2074f85c9
MattRemy <m.remigio331@gmail.com>
2026-05-15 21:00:33 -0700

    Add FamGrab route to app router

--- FamHelpDeskWebsite/src/FamHelpDesk.js
+++ FamHelpDeskWebsite/src/FamHelpDesk.js
@@ -7,9 +7,12 @@ import EditProfilePage from "./pages/user/EditProfilePage";
 import FamilyPageWrapper from "./components/family/FamilyPageWrapper";
 import CreateFamilyPage from "./pages/family/CreateFamilyPage";
 import FindFamilyPage from "./pages/family/FindFamilyPage";
+import ManageMembersPage from "./pages/family/ManageMembersPage";
 import GroupDetail from "./components/group/GroupDetailWrapper";
 import QueueDetail from "./components/queue/QueueDetailWrapper";
 import NotificationsPage from "./components/notifications/NotificationsPage";
+import FamGrabPage from "./pages/FamGrab/FamGrabPage";
+import UserReviewHistory from "./components/FamGrab/UserReviewHistory";
 import NotFoundPage from "./pages/NotFoundPage";
 import Navbar from "./components/Navbar";
 
@@ -31,12 +34,24 @@ const FamHelpDesk = () => {
             <Route path="/family/find" element={<FindFamilyPage />} />
             <Route path="/family/:familyId" element={<FamilyPageWrapper />} />
             <Route
+              path="/family/:familyId/manage-members"
+              element={<ManageMembersPage />}
+            />
+            <Route
               path="/family/:familyId/group/:groupId"
               element={<GroupDetail />}
             />
             <Route
               path="/family/:familyId/queue/:queueId"
               element={<QueueDetail />}
+            />
+            <Route
+              path="/family/:familyId/grab"
+              element={<FamGrabPage />}
+            />
+            <Route
+              path="/family/:familyId/grab/reviews/:userId"
+              element={<UserReviewHistory />}
             />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
